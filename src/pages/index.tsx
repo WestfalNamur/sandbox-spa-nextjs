fbb33833212d5b131fc45a2d913660f8a7a5be60
Stefan Crummenerl <crummenerls@gmail.com>
2022-10-31 17:43:19 +0100

    ADD: Tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Counter from "../components/lib/counter/Counter";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl text-center font-bold underline ">Hello world!</h1>
+    <>
+      <h1 className="text-3xl text-center font-bold underline ">
+        Hello world!
+      </h1>
+      <Counter />
+    </>
   );
 }
