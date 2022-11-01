9b99e4e4c2929d9b2d31624d581a2c1b211bbc83
Stefan Crummenerl <crummenerls@gmail.com>
2022-11-01 18:55:56 +0100

    ADD: Jest and React-testing-libray

---
+++ src/components/lib/counter/Counter.tsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={"flex flex-row"}>
+      <p className={"p-2"}>Count: {count}</p>
+      <p className={"p-2"} onClick={() => setCount(count + 1)}>Increment</p>
+    </div>
+  )
+}
+
+export default Counter
