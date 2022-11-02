46aab750b1bcd2a0f8f9f0ea3034f4cf071c0ba7
Stefan Crummenerl <37296211+WestfalNamur@users.noreply.github.com>
2022-11-02 22:25:37 +0100

    Revert "Testing"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import Counter from "../components/lib/counter/Counter";
-
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl text-center font-bold underline ">
-        Hello world!
-      </h1>
-      <Counter />
-    </>
+    <h1 className="text-3xl text-center font-bold underline ">Hello world!</h1>
   );
 }
